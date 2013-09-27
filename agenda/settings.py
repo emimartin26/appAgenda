20250bcd3f77e934191ef4f556cfefabc5335247
emimartin26@hotmail.com <emimartin26@hotmail.com>
2013-09-27 14:38:43 -0300

    ultimo commit de prueba

--- agenda/settings.py
+++ agenda/settings.py
@@ -22,12 +22,12 @@ MANAGERS = ADMINS
 #}
 
 DATABASES = {
-   'default': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dbAgenda',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
-        'PASSWORD': ''
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -35,7 +35,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
