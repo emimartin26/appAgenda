eff0e5880d61f633ea5c728872cdf13b19f6ed16
emimartin26@hotmail.com <emimartin26@hotmail.com>
2013-10-14 17:36:15 -0300

    cambie fecha del setyng.py de es-AR a es-MX

--- agenda/settings.py
+++ agenda/settings.py
@@ -50,7 +50,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'es-AR'
+LANGUAGE_CODE = 'es-MX'
 
 SITE_ID = 1
 
